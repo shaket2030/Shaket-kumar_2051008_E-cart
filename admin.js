3c2cdc0a74e579684bf1e315caabcef14f82ff06
shaket2030 <shaketjh123@gmail.com>
2024-06-23 17:45:40 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,11 +1,11 @@
 document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent default form submission
+    event.preventDefault(); 
 
-    // Get input values
+    
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
 
-    // Fetch admin data from JSON file
+    
     fetch('admin.json')
         .then(response => response.json())
         .then(data => {
@@ -19,14 +19,14 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
                 }
             }
 
-            // Display login status
+            
             var loginStatus = document.getElementById('loginStatus');
             if (loginSuccessful) {
                 localStorage.setItem('isLoggedIn', 'true');
-                // Redirect to admin panel on successful login
+               
                 window.location.href = 'admin1.html';
                 history.replaceState(null, '', 'admin1.html');
-               // window.history.replaceState({}, document.title, 'admin1.html');
+               
             } else {
                 loginStatus.textContent = 'Login failed. Please try again.';
                 loginStatus.style.color = 'red';
