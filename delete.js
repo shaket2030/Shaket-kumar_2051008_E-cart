305cdc801f02528e9847b60d0799cf1c695ba39a
shaket2030 <shaketjh123@gmail.com>
2024-07-15 08:37:41 +0530

    Update delete.js

--- delete.js
+++ delete.js
@@ -1,9 +1,9 @@
 document.getElementById('deleteForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent default form submission
+    event.preventDefault();
 
     var productId = document.getElementById('productId').value;
 
-    // Send a DELETE request to delete the product
+    
     fetch(`/api/products/${productId}`, {
         method: 'DELETE',
         headers: {
@@ -17,13 +17,13 @@ document.getElementById('deleteForm').addEventListener('submit', function(event)
         return response.json();
     })
     .then(deletedProduct => {
-        // Handle successful deletion
+        
         console.log('Product deleted successfully:', deletedProduct.message);
         alert('Product deleted successfully!');
-        window.location.href = '/index.html'; // Redirect to index.html after successful deletion
+        window.location.href = '/index.html'; 
     })
     .catch(error => {
-        // Handle fetch error or server-side error
+        
         console.error('Delete failed:', error);
         alert('Failed to delete product. Please try again.');
     });
