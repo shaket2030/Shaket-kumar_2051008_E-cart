78d7e7dd2b5ff20cb72c10adfa81d2771387653b
shaket2030 <shaketjh123@gmail.com>
2024-07-15 09:10:56 +0530

    Update update1.js

--- update1.js
+++ update1.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-// Read the JSON file
+
 fs.readFile('products.json', 'utf8', (err, data) => {
   if (err) {
     console.error(err);
@@ -8,18 +8,18 @@ fs.readFile('products.json', 'utf8', (err, data) => {
   }
 
   try {
-    // Parse the JSON data
+   
     const jsonData = JSON.parse(data);
 
-    // Add 'id' field with auto-incrementing values
+    
     jsonData.forEach((item, index) => {
       item.id = index + 1;
     });
 
-    // Convert back to JSON string
+    
     const updatedJsonData = JSON.stringify(jsonData, null, 2);
 
-    // Write the updated JSON data back to the file
+   
     fs.writeFile('product.json', updatedJsonData, 'utf8', (err) => {
       if (err) {
         console.error(err);
